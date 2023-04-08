47c9c039589078c45f26e47269c2d488fc14c9d5
Wojciech Jarosz <jaroszw@gmail.com>
2023-04-08 08:01:50 +0200

    CORS problem

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 3500;
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+
+app.use(logger);
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 // app.use(express.static("public"));
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use("/", require("./routes/root"));
+app.use('/', require('./routes/root'));
 
-app.all("*", (req, res) => {
+app.all('*', (req, res) => {
   res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 not found" });
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 not found' });
   } else {
-    res.type("txt").send("404 Not Found");
+    res.type('txt').send('404 Not Found');
   }
 });
 
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
