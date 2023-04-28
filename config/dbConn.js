ad9bb33a0e6920b077878ef94bae13064c384015
Jarosz Wojciech <jaroszw@gmail.com>
2023-04-28 13:09:04 +0200

    MongoDB config

---
+++ config/dbConn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+  try {
+    mongoose.connect(process.env.DATABASE_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,38 +1,50 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const { logger } = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents, logger } = require("./middleware/logger");
+
+connectDB();
 
 app.use(logger);
-
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
 app.use(cookieParser());
 
 // app.use(express.static("public"));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
 
-app.use('/', require('./routes/root'));
-
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
   res.status(404);
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 not found' });
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
   } else {
-    res.type('txt').send('404 Not Found');
+    res.type("txt").send("404 Not Found");
   }
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MONGO DB");
+  app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.dir(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
